e2071ec76e76d9c76eb33ab0b98b6098062a3688
bal3000 <bal3000286@googlemail.com>
2020-02-26 21:23:21 +0000

    admin

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,9 @@
+import { Repository, EntityRepository } from "typeorm";
+import { User } from "./entities/user.entity";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+    public async signUp() {
+
+    }
+}
