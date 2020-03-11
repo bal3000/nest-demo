b20480dacb9e9bcffe7e5b8467ae6018c48fcf86
bal3000 <bal3000286@googlemail.com>
2020-03-11 23:05:05 +0000

    signup

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,3 +1,4 @@
 export class AuthCredentialsDto {
-    
+    username: string;
+    password: string;
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,9 +1,17 @@
-import { Repository, EntityRepository } from "typeorm";
-import { User } from "./entities/user.entity";
+import { Repository, EntityRepository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-    public async signUp() {
+    public async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredentialsDto;
+        const user = new User();
+        user.username = username;
+        user.password = password;
 
+        await user.save();
     }
+
+    
 }
