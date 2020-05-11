5c76773637b553e24e1a974e4c59b6f1eb243a2a
bal3000 <bal3000286@googlemail.com>
2020-05-11 08:32:50 +0100

    configuration

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -5,13 +5,14 @@ import { JwtPayload } from '../models/jwt-payload.interface';
 import { UserRepository } from '../user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../entities/user.entity';
+import * as config from 'config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: 'topSecret51'
+            secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret')
         });
     }
 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
+  const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
+  const port = process.env.PORT || serverConfig.port; //PORT=3000 start:dev
+
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
